c785763723ff783a052bcaa62db3ef7b4b993562
Sam L <lafell.samuel@gmail.com>
2021-07-05 16:36:52 -0500

    Updates

--- playground/views.py
+++ playground/views.py
@@ -7,10 +7,17 @@ from django.http import HttpResponse
 # in django, it's called a 'view'
 # but in Django, the normal 'view' is called a 'template'
 
-def say_hello(request):
+#def say_hello(request):
     # Pull data
     # Transform Data
     # Send Email
     # For now, response
-    return HttpResponse('Hello World')
+ #   return HttpResponse('Hello World')
     # Now need to map it to a View
+
+# How can we use a template to return HTML content to the client?
+# Add folder , 'templates', and add file
+
+# Render a template and return HTML markup
+def say_hello(request):
+    return render(request, 'hello.html')
