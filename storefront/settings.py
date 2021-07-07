24cda5deb0d2387c34372f7832ee6b47fc81d236
Sam L <lafell.samuel@gmail.com>
2021-07-07 18:51:49 -0500

    Update settings.py

--- storefront/settings.py
+++ storefront/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
@@ -127,5 +127,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = [
-    '127.0.0.1'
+    '192.168.100.13',
 ]
